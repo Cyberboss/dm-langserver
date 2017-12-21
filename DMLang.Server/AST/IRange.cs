9897368ec6935e466a9cc2c98889ad202f114354
Cyberboss <Cyberboss@users.noreply.github.com>
2017-12-21 09:58:56 -0500

    Improve IRange

--- DMLang.Server/AST/IRange.cs
+++ DMLang.Server/AST/IRange.cs
@@ -3,9 +3,10 @@
 namespace DMLang.Server.AST
 {
 	/// <summary>
+	/// (x to y)
 	/// FUCK THIS FUCKING WEIRDASS MAGICAL BULLSHIT WHAT IS IT??!?!?!?!
 	/// </summary>
-	interface IRange : IConstantValue, IExpression
+	interface IRange : IConstantValue
 	{
 		IReadOnlyList<IConstantValue> Items { get; }
 	}
