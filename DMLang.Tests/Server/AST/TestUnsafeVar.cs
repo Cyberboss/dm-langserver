72a609cc4d7fe10e7b2bb3d2c9793e1d6dd66b8d
Cyberboss <Cyberboss@users.noreply.github.com>
2017-12-21 14:45:29 -0500

    Skeletion UnsafeVar class

---
+++ DMLang.Tests/Server/AST/TestUnsafeVar.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DMLang.Server.AST.Tests
+{
+	/// <summary>
+	/// Tests for <see cref="UnsafeVar"/>
+	/// </summary>
+	[TestClass]
+	public sealed class TestUnsafeVar
+	{
+		[TestMethod]
+		public void TestConstruction()
+		{
+			var first = new UnsafeVar("Name", null);
+			Assert.AreEqual("Name", first.Name);
+			Assert.IsNull(first.Owner);
+			var second = new UnsafeVar("Name", first);
+			Assert.AreSame(first, second.Owner);
+			Assert.ThrowsException<ArgumentNullException>(() => new UnsafeVar(null, null));
+			Assert.ThrowsException<ArgumentNullException>(() => new UnsafeVar(null, first));
+		}
+	}
+}
