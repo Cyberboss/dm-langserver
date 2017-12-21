885a53a7837453a87c20d659d871d1e028d7b3d7
Cyberboss <Cyberboss@users.noreply.github.com>
2017-12-21 14:26:29 -0500

    First AST test

---
+++ DMLang.Tests/Server/AST/TestAST.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DMLang.Server.AST.Tests
+{
+	/// <summary>
+	/// Tests for <see cref="AST"/>
+	/// </summary>
+	[TestClass]
+	public sealed class TestAST
+	{
+		[TestMethod]
+		public void TestConstruction()
+		{
+			new AST();
+		}
+	}
+}
